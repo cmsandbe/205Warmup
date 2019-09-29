5bcb4adb57525886527a8c59ef51fc7545d95c9c
Prasidha Timsina <prasidhatimsina@gmail.com>
2019-09-29 11:15:28 -0400

    added query statements

--- Query.java
+++ Query.java
@@ -5,10 +5,41 @@ public class Query {
       SqliteDB database = new SqliteDB();
       
       String queryStatementExample = "SELECT * FROM Poke WHERE Name = 'Charmeleon';";
-      String queryStatementExample2 = "SELECT * FROM Type WHERE typeid = '5';";
+      String queryStatementExample2 = "      SELECT Poke.TypeID, Poke.Name, Stat.MaxCP,Stat.MaxHP\n" + 
+      		"      FROM Poke\n" + 
+      		"      INNER JOIN Stat\n" + 
+      		"      ON Poke.typeid=Stat.typeid WHERE Name = 'Charmeleon';  ";
+      														//just change charmeleon to var of pokemon name
+      String queryStatementExample3 = "      SELECT Poke.Name, Type.typename\n" + 
+      		"      FROM Poke\n" + 
+      		"      INNER JOIN Type\n" + 
+      		"      ON Poke.t1id=Type.typeid WHERE Name = 'Charmeleon';";
+      					  //change t1id to t2id if they want secondary id, and change charmeleon to var of pokemon name
+
 
       database.runQuery(queryStatementExample);
+      database.runStatsQuery(queryStatementExample2);
+      database.runTypeQuery(queryStatementExample3);
+
 //      database.runQuery(queryStatementExample2);
+      
+      //query for join requirement: 
+//      SELECT Poke.TypeID, Poke.Name, Stat.MaxCP,Stat.MaxHP
+//      FROM Poke
+//      INNER JOIN Stat
+//      ON Poke.typeid=Stat.typeid WHERE Name = 'Charmeleon';      
+      //this returns '5' 'charmeleon' '1568' '103'
+      
+//      
+//      SELECT Poke.Name, Type.typename
+//      FROM Poke
+//      INNER JOIN Type
+//      ON Poke.t1id=Type.typeid WHERE Name = 'Charmeleon';
+      
+      //this returns 'charmeleon' 'fighting'
+      
+      
+      
 
       database.closeConnection();
       
