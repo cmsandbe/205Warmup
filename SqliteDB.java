4f5c10a098b349e62206b29e9cdf8bab87e5d5d2
cmsandbe <38229227+cmsandbe@users.noreply.github.com>
2019-09-29 21:50:11 -0400

    Update SqliteDB.java
    replaced entire constructor, created and filled createTables() method - Colin

--- SqliteDB.java
+++ SqliteDB.java
@@ -3,18 +3,120 @@ import java.sql.*;
 public class SqliteDB{
     Connection connection = null;
     Statement statement = null;
+    String url = "";
 
-    SqliteDB(){
-        //we connect to db here
-        try {
-            Class.forName("org.sqlite.JDBC");
-            //warmup.sqlite is the name of our database .sqlite file
-            connection=DriverManager.getConnection("jdbc:sqlite:warmup.sqlite");
-            //if we got here, we connected
-            System.out.println("Connection successful");
-//            connection.close();
-        } catch (Exception e) {
-            System.out.println("Error message: " + e.getMessage());
+    SqliteDB(String fileName){
+    	// Code for this connection is from https://www.sqlitetutorial.net/sqlite-java/create-database/
+    	 url = "jdbc:sqlite:" + System.getProperty("user.dir") + "/" + fileName;  // Make connection URL
+    	 try (Connection conn = DriverManager.getConnection(url)) {  // Try connecting to DB, create new if needed
+             if (conn != null) {
+                 DatabaseMetaData meta = conn.getMetaData();
+                 System.out.println("The driver name is " + meta.getDriverName());
+                 System.out.println("A new database has been created.");
+             }
+         } catch (SQLException e) {  // Catch exceptions and print the error message
+             System.out.println(e.getMessage());
+         }
+    }
+    
+    public void createTables() throws IOException{
+    	
+        // SQL statement for creating a new table created as a string
+        String sql ="CREATE TABLE IF NOT EXISTS Poke (\n"
+                + "    TypeID int PRIMARY KEY,\n"
+                + "    Name varchar NOT NULL,\n"
+                + "    T1ID int NOT NULL,\n"
+                + "    T2ID int,\n"
+                + "    imageurl varchar(200)\n"
+                + ");";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);  // Test the connection again and then try the statement
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());  // catch any errors and print out message
+        }
+
+        // Create a file and scanner for that file
+        File pokemoncsv = new File(System.getProperty("user.dir") + "/"+ "tblPokemon.csv");
+        Scanner r = new Scanner(pokemoncsv);
+        sql = "";
+        for(int i = 0; r.hasNext(); i++) {  // For each line in the file, insert into table
+        	String[] line =  r.nextLine().split(",");
+        	sql += "INSERT INTO Poke (TypeID, Name, T1ID, T2ID, imageurl)\n"
+        			+ "values ('" + line[0] + "', '" + line[1].trim() + "', '" + line[2] + "', '" 
+        			+ line[3] + "', '" + line[4]+ "'); \n";
+            // System.out.println(sql);  // print out statement to error proof           
+            try (Connection conn = DriverManager.getConnection(url);
+                    Statement stmt = conn.createStatement()) {
+                stmt.execute(sql);  // Connect again and try the statement
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());  // print out any errors that you get
+            }
+            sql = "";
+        }
+
+        // Using same code from above, create new table
+        sql = "CREATE TABLE IF NOT EXISTS Stats(\n"
+                + "    TypeID integer PRIMARY KEY,\n"
+                + "    MaxCP int,\n"
+                + "    MaxHP int \n"
+                + ");";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);  // Execute the new table statement
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        // Change the file and scanner to the next file Repeat same steps
+        pokemoncsv = new File(System.getProperty("user.dir") + "/"+ "tblStats.csv");
+        r = new Scanner(pokemoncsv);
+        sql = "";
+        for(int i = 0; r.hasNext(); i++) {  // Get data from each line and insert into table
+        	String[] line =  r.nextLine().split(",");
+        	sql += "INSERT INTO Stats(TypeID, MaxCP, MaxHP)\n"
+        			+ "values ('" + line[0] + "', '" + line[1] + "', '" + line[2] + "'); \n";
+            // System.out.println(sql);  // Print out statement to error proof
+            try (Connection conn = DriverManager.getConnection(url);
+                    Statement stmt = conn.createStatement()) {
+                stmt.execute(sql);  // execute statement
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());  // Catch errors and print them out
+            }
+            sql = "";
+        }
+
+        // Repeat steps from above for last table
+        sql = "CREATE TABLE IF NOT EXISTS Types(\n"
+                + "    typeID int PRIMARY KEY,\n"
+                + "    typeName varchar(20)\n"
+                + ");";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);  // execute the new table statement
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        // Set the file and scanner to the new file and repeat steps from above
+        pokemoncsv = new File(System.getProperty("user.dir") + "/"+ "tblTypes.csv");
+        r = new Scanner(pokemoncsv);
+        sql = "";
+        for(int i = 0; r.hasNext(); i++) {  // for each line in the file get the data and insert into table
+        	String[] line =  r.nextLine().split(",");
+        	sql += "INSERT INTO Types(TypeID, typeName)\n"
+        			+ "values ('" + line[0] + "', '" + line[1] + "'); \n";
+            // System.out.println(sql);  // Print out the statement to error proof
+            try (Connection conn = DriverManager.getConnection(url);
+                    Statement stmt = conn.createStatement()) {
+                stmt.execute(sql);  // Connect and then execute the statement
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());  // Catch any errors and print them out
+            }
+            sql = "";
         }
     }
     
